3d1ff317c6ec9d1fbb4a1c2c2beed6766ae28bdc
Ayush Bhagat <ayushbhagat@gmail.com>
2018-02-07 17:32:14 -0500

    Change Makefile and joosc to use java instead of kotlin to run and also change joosc to forward what the java program returns as its exit status.

--- Makefile
+++ Makefile
@@ -5,13 +5,13 @@ default: build convert_dfa_regex_to_string create_scanner_dfa
 build:
 	mkdir -p gen
 	chmod +x joosc
-	kotlinc src/main/* -d gen/oj.jar
+	kotlinc src/main/* -include-runtime -d gen/oj.jar
 
 convert_dfa_regex_to_string:
-	kotlin -cp gen/oj.jar oj.scripts.ConvertDFARegexToStringKt
+	java -cp gen/oj.jar oj.scripts.ConvertDFARegexToStringKt
 
 create_scanner_dfa:
-	kotlin -cp gen/oj.jar oj.scripts.CreateScannerDFAKt
+	java -cp gen/oj.jar oj.scripts.CreateScannerDFAKt
 
 clean:
 	rm -rf gen
--- src/main/kotlin/oj/OJ.kt
+++ src/main/kotlin/oj/OJ.kt
@@ -2,37 +2,35 @@ import oj.models.NFA
 import oj.scanner.BASE_DFA_NAMES
 import oj.scanner.SCANNER_DFA
 import oj.scanner.Scanner
-import oj.scanner.ScannerError
 import java.io.File
-import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         System.exit(42)
     }
-    var inputFileString = File(args[0]).inputStream().bufferedReader().use { it.readText() }
-    var baseDfas = BASE_DFA_NAMES
-            .keys
-            .map {
-                NFA.deserialize(
-                        "gen/$it.dfa",
-                        NFA.EmptyStateDataHelper(),
-                        oj.scanner.ALPHABET,
-                        it)
-            }
-            .toSet()
-    val scannerDfa = NFA.deserialize(
-            "gen/$SCANNER_DFA.dfa",
-            NFA.EmptyStateDataHelper(),
-            oj.scanner.ALPHABET,
-            "")
-    val scanner = Scanner(scannerDfa, baseDfas)
     try {
+        var inputFileString = File(args[0]).inputStream().bufferedReader().use { it.readText() }
+        var baseDfas = BASE_DFA_NAMES
+                .keys
+                .map {
+                    NFA.deserialize(
+                            "gen/$it.dfa",
+                            NFA.EmptyStateDataHelper(),
+                            oj.scanner.ALPHABET,
+                            it)
+                }
+                .toSet()
+        val scannerDfa = NFA.deserialize(
+                "gen/$SCANNER_DFA.dfa",
+                NFA.EmptyStateDataHelper(),
+                oj.scanner.ALPHABET,
+                "")
+        val scanner = Scanner(scannerDfa, baseDfas)
         scanner.tokenize(inputFileString)
-        println("Scanner Passes")
-    } catch (e: ScannerError) {
-        println("Scanner Error")
+    } catch (e: Exception) {
+        println(e.message)
         System.exit(42)
     }
+    println("Passes")
     System.exit(0)
 }
