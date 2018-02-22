6b5d12e4821bf996d381523f9f152ab34643e930
Ramanpreet Nara <ramanpreetnara@gmail.com>
2018-02-22 16:08:11 -0500

    Find all usages of TypeNames.

--- src/main/kotlin/oj/models/CSTNode.kt
+++ src/main/kotlin/oj/models/CSTNode.kt
@@ -2,13 +2,63 @@ package oj.models
 
 import java.util.*
 
+class FoundNoChild: Exception("Found no descendants, expected 1.")
+class FoundManyChildren: Exception("Found many descendants, expected 1.")
+
+class FoundNoDescendant : Exception("Found no descendants, expected 1.")
+class FoundManyDescendants: Exception("Found many descendants, expected 1.")
+
 data class CSTNode(
     val name: String,
     val lexeme: String,
     val children: MutableList<CSTNode> = mutableListOf()
 ) {
+
+    fun getDescendant(name: String): CSTNode {
+        return getDescendant({ it.name == name })
+    }
+
+    fun getChild(name: String): CSTNode {
+        return getChild({ it.name == name })
+    }
+
+    fun getChildren(name: String): List<CSTNode> {
+        return getChildren({ it.name == name })
+    }
+
     fun getDescendants(name: String): List<CSTNode> {
         return getDescendants({ it.name == name})
+    }
+
+    fun getChild(predicate: (CSTNode) -> Boolean): CSTNode {
+        val matchingChildren = getChildren(predicate)
+
+        if (matchingChildren.isEmpty()) {
+            throw FoundNoChild()
+        }
+
+        if (matchingChildren.size > 1) {
+            throw FoundManyChildren()
+        }
+
+        return matchingChildren[0]
+    }
+
+    fun getDescendant(predicate: (CSTNode) -> Boolean): CSTNode {
+        val descendants = getDescendants(predicate)
+        if (descendants.isEmpty()) {
+            throw FoundNoDescendant()
+        }
+
+        if (descendants.size > 1) {
+            throw FoundManyDescendants();
+        }
+
+        return descendants[0]
+    }
+
+    fun getChildren(predicate: (CSTNode) -> Boolean) : List<CSTNode> {
+        return children.filter(predicate)
     }
 
     fun getDescendants(predicate: (CSTNode) -> Boolean): List<CSTNode> {
--- src/main/kotlin/oj/nameresolver/NameResolver.kt
+++ src/main/kotlin/oj/nameresolver/NameResolver.kt
@@ -2,6 +2,8 @@ package oj.nameresolver
 
 import oj.models.CSTNode
 import oj.models.CSTNodeVisitor
+import oj.models.FoundNoChild
+import oj.models.FoundNoDescendant
 
 class InvalidNameResolutionInsertion(reason: String): Exception(reason)
 
@@ -25,6 +27,151 @@ class NameResolutionVisitor(private val packages: Map<String, Set<CSTNode>>) : C
         }
 
         nameResolution[nameNode] = declaration
+    }
+
+    /**
+     * TypeName:
+     * In a single-type-import declaration (§7.5.1)
+     */
+    override fun visitSingleTypeImportDeclaration(node: CSTNode) {
+        val nameNode = node.getChild("Name")
+    }
+
+    /**
+     * TypeName:
+     * In an extends clause in a class declaration (§8.1.3)
+     */
+    override fun visitSuper(node: CSTNode) {
+        val nameNode = node.getDescendant("Name")
+    }
+
+    /**
+     * TypeName:
+     * In an implements clause in a class declaration (§8.1.4)
+     * In an extends clause in an interface declaration (§9.1.2)
+     */
+    override fun visitInterfaceType(node: CSTNode) {
+        val nameNode = node.getDescendant("Name")
+    }
+
+    /**
+     * TypeName:
+     * As a Type In a field declaration (§8.3, §9.3)
+     */
+    override fun visitFieldDeclaration(node: CSTNode) {
+        val typeNode = node.getChild("Type")
+
+        try {
+            val nameNode = typeNode.getDescendant("Name")
+
+        } catch (ex: FoundNoDescendant) {
+            super.visitFieldDeclaration(node)
+        }
+    }
+
+    /**
+     * TypeName:
+     * As the result type of a method (§8.4, §9.4)
+     */
+    override fun visitMethodHeader(node: CSTNode) {
+        try {
+            val typeNode = node.getChild("Type")
+            val nameNode = typeNode.getDescendant("Name")
+
+
+        } catch (ex: FoundNoChild) {
+            super.visitMethodHeader(node)
+        } catch (ex: FoundNoDescendant) {
+            super.visitMethodHeader(node)
+        }
+    }
+
+    /**
+     * TypeName:
+     * As the type of a formal parameter of a method or constructor
+     * (§8.4.1, §8.8.1, §9.4)
+     */
+    override fun visitFormalParameter(node: CSTNode) {
+        val typeNode = node.getChild("Type")
+
+        try {
+            val nameNode = typeNode.getDescendant("Name")
+
+
+        } catch (ex: FoundNoDescendant) {
+            super.visitFormalParameter(node)
+        }
+    }
+
+    /**
+     * TypeName:
+     * As the type of a local variable (§14.4)
+     */
+    override fun visitLocalVariableDeclaration(node: CSTNode) {
+        val typeNode = node.getChild("Type")
+
+        try {
+            val nameNode = typeNode.getDescendant("Name")
+
+
+        } catch (ex: FoundNoDescendant) {
+            super.visitLocalVariableDeclaration(node)
+        }
+    }
+
+    /**
+     * TypeName:
+     * As the class type which is to be instantiated in an unqualified class instance
+     * creation expression (§15.9)
+     */
+    override fun visitClassInstanceCreationExpression(node: CSTNode) {
+        val classTypeNode = node.getChild("ClassType")
+        val nameNode = classTypeNode.getDescendant("Name")
+    }
+
+    /**
+     * TypeName:
+     * As the element type of an array to be created in an array creation expression (§15.10)
+     */
+    override fun visitArrayCreationExpression(node: CSTNode) {
+        try {
+            val classOrInterfaceTypeNode = node.getChild("ClassOrInterfaceType")
+            val nameNode = classOrInterfaceTypeNode.getChild("Name")
+
+        } catch (ex: FoundNoChild) {
+            super.visitArrayCreationExpression(node)
+        }
+    }
+
+    /**
+     * TypeName:
+     * As the type mentioned in the cast operator of a cast expression (§15.16)
+     */
+    override fun visitCastExpression(node: CSTNode) {
+        val nameNode = when (node.children[1].name) {
+            "Name" -> node.children[1]
+            "Expression" -> node.children[1].getDescendant("Name")
+            else -> null
+        }
+
+        if (nameNode == null) {
+            super.visitCastExpression(node)
+            return
+        }
+    }
+
+    /**
+     * TypeName:
+     * As the type that follows the instanceof relational operator (§15.20.2)
+     */
+    override fun visitRelationalExpression(node: CSTNode) {
+        try {
+            val referenceTypeNode = node.getChild("ReferenceType")
+            val nameNode = referenceTypeNode.getDescendant("Name")
+
+        } catch (ex: FoundNoChild) {
+            super.visitRelationalExpression(node)
+        }
     }
 }
 
